27264fae07bd9d3fc100c7d5e37d785d0b5cacef
Kate <kate.dovzhenk@gmail.com>
2025-01-17 17:38:55 -0500

    debug

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -13,16 +13,3 @@ export const createGallery = imgInfo => {
             </div>
           </li>`;
 };
-
-// export const createGallery = gallery.addEventListener('click', event => {
-//   event.preventDefault();
-//   const clickedImg = event.target;
-//   if (clickedImg.tagName !== 'IMG') return;
-//   const largeImageUrl = clickedImg.dataset.source;
-//   const modal = basicLightbox.create(`
-//   <li class="gallery-card">
-//             <img class="gallery-img" src="${imgInfo.webformatURL}" alt="${imgInfo.tags}" />
-//           </li>`);
-
-//   modal.show();
-// });
